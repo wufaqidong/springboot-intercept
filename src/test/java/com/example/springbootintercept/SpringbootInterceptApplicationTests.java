f14efa33a1f4e16a83e29b89d51e89470db869d8
xiebinbin <513614809@qq.com>
2022-07-29 13:45:28 +0800

    Initial commit

---
+++ src/test/java/com/example/springbootintercept/SpringbootInterceptApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.springbootintercept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description:
+ * @Param:
+ * @return:
+ * @Author: xiebinbin
+ * @Date: 2022/7/29
+ */
+@SpringBootTest
+class SpringbootInterceptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
